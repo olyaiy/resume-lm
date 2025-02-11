b17de43c4cdd88d6b231a27b0754ff917fa5454c
olyaiy <alexolyaiy@gmail.com>
2025-02-11 15:11:46 -0800

    improve landing page

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -63,7 +63,7 @@ export default async function LoginPage({
   const showErrorDialog = params?.error === 'email_confirmation';
 
   return (
-    <main className="relative overflow-x-hidden selection:bg-violet-200/50">
+    <main className="relative overflow-x-hidden selection:bg-violet-200/50 -my-14">
       {/* Error Dialog */}
       <ErrorDialog isOpen={!!showErrorDialog} />
 
--- src/components/cover-letter/cover-letter.tsx
+++ src/components/cover-letter/cover-letter.tsx
@@ -8,6 +8,7 @@ import { useToast } from "@/hooks/use-toast";
 
 interface CoverLetterProps {
     containerWidth: number;
+    
 }
 
 
