7843f9643face3ea93fa43c557390e4f2419dbe6
olyaiy <alexolyaiy@gmail.com>
2025-05-18 20:10:35 -0700

    remove

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,9 @@ const nextConfig: NextConfig = {
       // ...
     },
   },
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
   productionBrowserSourceMaps: false,
   reactStrictMode: false,
 }
