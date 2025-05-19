066c4cbc743f3be852369ac697ef3f389b48d2b2
olyaiy <alexolyaiy@gmail.com>
2025-05-18 19:42:43 -0700

    updatesnpm

--- src/app/api/webhooks/stripe/route.ts
+++ src/app/api/webhooks/stripe/route.ts
@@ -6,7 +6,7 @@ import { manageSubscriptionStatusChange } from '@/utils/actions/stripe/actions'
 import { createServiceClient } from '@/utils/supabase/server'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-12-18.acacia'
+  apiVersion: '2025-04-30.basil'
 })
 
 const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET!
